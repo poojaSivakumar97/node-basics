79f76e824db2be5578c0e3c2ab1af04795d0c8a4
Pooja <poojasivakumar97@gmail.com>
2023-05-30 09:31:13 +0530

    feat: Mongodb CRUD ops

--- mongoDb/index.js
+++ mongoDb/index.js
@@ -14,6 +14,7 @@ const courseSchema = new mongoose.Schema({
   creator: String,
   publishedDate: { type: Date, default: Date.now },
   isPublished: Boolean,
+  rating: Number,
 });
 
 //   model
@@ -23,12 +24,88 @@ const Course = mongoose.model("Course", courseSchema);
 async function addCourse() {
   const course = new Course({
     name: "DBMS",
-    creator: "Ishan",
+    creator: "Kranthi",
     isPublished: false,
+    rating: 3.8,
   });
 
   const result = await course.save();
   console.log(result);
 }
 
-addCourse();
+// addCourse();
+
+/** get a particular course or filter out courses based on condition */
+
+async function getCourses() {
+  const courses = await Course.find({ isPublished: true })
+    .select({
+      name: 1,
+      publishedDate: 1,
+    })
+    .sort({ name: -1 }); // -1 means descending
+  console.log(courses);
+}
+// getCourses();
+
+// comparison operators
+
+/** eq =equal to
+ * gt(greater than )
+ * gte(greater than and equal to)
+ * lt(less than)
+ * lte(less than and equal to)
+ *
+ *
+ * in and not in --see documents in particular range
+ */
+
+async function getCourses1() {
+  const courses = await Course.find({
+    rating: {
+      $in: [3.2, 4.5],
+    },
+  }).sort({ name: 1 });
+  console.log(courses);
+}
+
+// getCourses1();
+
+/**  LOgical operators
+ *  and
+ * or
+ */
+
+async function getCourses2() {
+  const courses = await Course.find({
+    rating: {
+      $in: [3, 4.5, 4.2],
+    },
+  })
+    .sort({ name: 1 })
+    .and([{ creator: "Mrinal" }, { rating: 2 }]);
+  console.log(courses);
+}
+
+// getCourses2();
+
+// Update existing course
+
+async function updateCourse(id) {
+  let course = await Course.findById(id);
+  if (!course) return;
+  course.name = "OOPS";
+  course.creator = "Shawn";
+  let updatedCourse = await course.save();
+  console.log(updatedCourse);
+}
+// updateCourse("6475694784d146f9b965a71d");
+
+// delete course
+
+async function deleteCourse(id) {
+  let course = await Course.findByIdAndRemove(id);
+  console.log(course);
+}
+
+deleteCourse("6475694784d146f9b965a71d");
